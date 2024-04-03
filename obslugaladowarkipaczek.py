a1dc9f379b8f1075381a691d1c0a1f1e6fa6a27f
judytstr <161215289+judytstr@users.noreply.github.com>
2024-04-03 15:24:51 +0200

    Update obslugaladowarkipaczek.py
    poprawna wesja

--- obslugaladowarkipaczek.py
+++ obslugaladowarkipaczek.py
@@ -1,12 +1,4 @@
 num_of_packages = int(input("Ilość paczek do wysłania: "))
-package_weights = []
-
-i = 0
-while i < num_of_packages:
-    weight = int(input(f"Podaj wagę paczki {i + 1} (od 1 do 10 kg): "))
-    if weight >= 1 and weight <= 10:
-        package_weights.append(weight)
-        i += 1
 
 sent_packages = 0
 sent_weight = 0
@@ -14,15 +6,19 @@ empty_weight = 0
 max_empty_weight = 0
 max_empty_package = 0
 
-for weight in package_weights:
-    while sent_weight + weight > 20:
-        sent_packages += 1
-        empty_weight += 20 - sent_weight
-        if 20 - sent_weight > max_empty_weight:
-            max_empty_weight = 20 - sent_weight
-            max_empty_package = sent_packages
-        sent_weight = 0
-    sent_weight += weight
+i = 0
+while i < num_of_packages:
+    weight = int(input(f"Podaj wagę paczki {i + 1} (od 1 do 10 kg): "))
+    if weight >= 1 and weight <= 10:
+        while sent_weight + weight > 20:
+            sent_packages += 1
+            empty_weight += 20 - sent_weight
+            if 20 - sent_weight > max_empty_weight:
+                max_empty_weight = 20 - sent_weight
+                max_empty_package = sent_packages
+            sent_weight = 0
+        sent_weight += weight
+        i += 1
 
 sent_packages += 1
 empty_weight += 20 - sent_weight
@@ -31,5 +27,4 @@ print("\nPodsumowanie:")
 print(f"Wysłano {sent_packages} paczek")
 print(f"Wysłano {sent_weight} kg")
 print(f"Suma pustych kilogramów: {empty_weight} kg")
-print(
-    f"Najwięcej pustych kilogramów ma paczka {max_empty_package} ({max_empty_weight} kg)")
+print(f"Najwięcej pustych kilogramów ma paczka {max_empty_package} ({max_empty_weight} kg)")
